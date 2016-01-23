3b9747325e6fa0d533a290fc2f127a2ae481b992
Corey Dixon <zimhey1@gmail.com>
2016-01-22 21:59:56 -0500

    StatsUI working

---
+++ Assets/UI/StatsUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatsUI : MonoBehaviour {
+
+    public Text health { get; set; }
+    public Text mana { get; set; }
+    public Text kills { get; set; }
+
+    // Use this for initialization
+    void Start () {
+        health = GameObject.Find("UIHealth").GetComponent<Text>();
+        mana = GameObject.Find("UIMana").GetComponent<Text>();
+        kills = GameObject.Find("UIKills").GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    
+}
