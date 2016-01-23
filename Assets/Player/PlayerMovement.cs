ab9eeff9393c516f5a02597f28ef44df4e4abc83
mdduran <mdduran@outlook.com>
2016-01-22 21:56:44 -0500

    Added bunch of character code and camera following

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 2.5f;
+    public float rotateSpeed = 2.5f;
+
+	
+	// Update is called once per frame
+	void Update () {
+        CharacterController charController = GetComponent<CharacterController>();
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 backward = transform.TransformDirection(Vector3.back);
+        float currentSpeed; 
+        /*
+        if (Input.GetAxis("Vertical") > 0){
+            currentSpeed = speed * Input.GetAxis("Vertical");
+            charController.SimpleMove(forward * currentSpeed);
+        }
+        if (Input.GetAxis("Vertical") < 0){
+            currentSpeed = speed * Input.GetAxis("Vertical");
+            charController.SimpleMove(backward * -currentSpeed);
+        }
+         */
+        currentSpeed = speed * Input.GetAxis("Vertical");
+        charController.Move(forward *currentSpeed);
+
+        
+	}
+}
