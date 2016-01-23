6e33d27bbd0e86292c413b82c227b0d94cf6ed79
mdduran <mdduran@outlook.com>
2016-01-23 00:52:29 -0500

    Fixed camera and movement controls, test scene with prefabs is added

--- Assets/Scripts/Player/CameraScript.cs
+++ Assets/Scripts/Player/CameraScript.cs
@@ -9,11 +9,12 @@ public class CameraScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.position = player.transform.position + new Vector3(-11.1f, 12.35f, 12.24f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.LerpUnclamped(transform.position, player.transform.position + new Vector3(-16.1f, 17, 16.5f),  Time.smoothDeltaTime);
+        transform.position = Vector3.LerpUnclamped(transform.position, player.transform.position + new Vector3(-16.1f, 17, 4),  Time.smoothDeltaTime);
         transform.rotation = Quaternion.Euler(45, 90, 0);
 	}
 }
