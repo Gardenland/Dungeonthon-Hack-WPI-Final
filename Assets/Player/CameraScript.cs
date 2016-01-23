d7e0325ee4a4f15f7d38cdf6d09c16b77f258680
mdduran <mdduran@outlook.com>
2016-01-22 21:07:11 -0500

    Added solution file and created isometric camera, starting camera tracking and player movement

---
+++ Assets/Player/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    //Need transform object
+    Transform player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
