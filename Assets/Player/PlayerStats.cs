8fa1e71a77cf61f2a3c5aaf20e7b8584703b67ce
Corey Dixon <zimhey1@gmail.com>
2016-01-22 22:29:00 -0500

    PlayerStats script will update the UI when stats are changed

---
+++ Assets/Player/PlayerStats.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+    private int hp;
+    private int mp;
+    private int kills;
+    private StatsUI ui;
+
+    public int Health
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            hp = value;
+            ui.Health = value;
+            if (hp <= 0)
+                ;// TODO Death method
+        }
+    }
+
+    public int Mana
+    {
+        get
+        {
+            return mp;
+        }
+        set
+        {
+            mp = value;
+            ui.Mana = value;
+        }
+    }
+
+    public int Kills
+    {
+        get
+        {
+            return kills;
+        }
+        set
+        {
+            kills = value;
+            ui.Kills = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        ui = GameObject.Find("UIStats").GetComponent<StatsUI>();
+        Health = Mana = Kills = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
