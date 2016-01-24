ec5fcaadd38ce7b1e273a1c2594d041b79b0302a
Corey Dixon <zimhey1@gmail.com>
2016-01-24 05:24:59 -0500

    HealthPickup Script

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickup : MonoBehaviour {
+
+    public int HealthValue;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject != null)
+        {
+            Stats stats = collider.gameObject.GetComponent<Stats>();
+            if (stats != null)
+                stats.Health += HealthValue;
+        }
+    }
+}
