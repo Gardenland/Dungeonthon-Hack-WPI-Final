da0c415c5d68951295411bcdd4f380b08c0e5152
Brianjkd <brianjkd@gmail.com>
2016-01-23 19:47:46 -0500

    Updated camera script

--- Assets/Scripts/Player/CameraScript.cs
+++ Assets/Scripts/Player/CameraScript.cs
@@ -5,16 +5,18 @@ public class CameraScript : MonoBehaviour {
 
     //public GameObject Camera;
     //Need transform object
+	public Vector3 playerOffset = new Vector3(-12f, 15f, 2f);
     Transform player;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        transform.position = player.transform.position + new Vector3(-11.1f, 12.35f, 12.24f);
+		transform.position = player.transform.position + playerOffset;
+		transform.rotation = Quaternion.Euler(45, 90, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.LerpUnclamped(transform.position, player.transform.position + new Vector3(-16.1f, 17, 4),  Time.smoothDeltaTime);
-        transform.rotation = Quaternion.Euler(45, 90, 0);
+		transform.position = Vector3.LerpUnclamped(transform.position, player.transform.position + playerOffset,  Time.smoothDeltaTime *2);
+        //transform.rotation = Quaternion.Euler(45, 90, 0);
 	}
 }
