ee07052f6e109b93b1a35d2fbef4b187f299da00
Corey Dixon <zimhey1@gmail.com>
2016-01-24 04:15:35 -0500

    DropItem component

---
+++ Assets/Scripts/DropItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropItem : MonoBehaviour {
+
+    public string Drop;
+    public int Percntage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDeath()
+    {
+        int num = Random.Range(0, 100);
+        if (num < Percntage)
+            Instantiate(Resources.Load(Drop), gameObject.transform.position, gameObject.transform.rotation);
+    }
+
+}
